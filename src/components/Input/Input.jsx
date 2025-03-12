814345aa44fc8bde3ddf0ae6412342266e912ed0
Remi Achji <achji.remi@gmail.com>
2025-03-12 19:05:06 +0400

    added floating label + new smartphone svg + svg plugin for vite

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -4,42 +4,67 @@ import { forwardRef } from "react";
 import { classNames } from "primereact/utils";
 import { IconField } from "primereact/iconfield";
 import { InputIcon } from "primereact/inputicon";
+import { FloatLabel } from "primereact/floatlabel";
+import PhoneIcon from "@images/smartphone.svg?react";
 
 const Input = forwardRef(
 	(
-		{ dialCode = "", onChange, onBlur, name, error, onClick, value, type, disabled = false },
+		{
+			dialCode = "",
+			onChange,
+			onBlur,
+			name,
+			error,
+			onClick,
+			value,
+			type,
+			disabled = false,
+		},
 		ref
 	) => {
 		return (
 			<div className={styles.container}>
-				<IconField iconPosition="left" className={styles.input_wrapper}>
-					<InputIcon className="pi pi-mobile"></InputIcon>
-					<InputMask
-						autoClear={false}
-						ref={ref}
-						name={name}
-						onChange={onChange}
-						onBlur={onBlur}
-						mask={`${dialCode}9 999 9999`}
-						placeholder="05"
-						className={classNames(styles.input, {
-							"p-invalid": error,
-						})}
-						onClick={onClick}
-						value={value}
-						type={type}
-						autoComplete={"tel"} // TODO > not needed for OTP input ?
-						inputMode={"tel"} // TODO > not needed for OTP input ?
-					/>
-					{!disabled && (
-						<div className={styles.checkmark_container}>
-							<i className={`pi pi-check-circle ${styles.checkmark_animation}`}></i>
-						</div>
-					)}
-				</IconField>
+				<FloatLabel pt={{ root: { style: { width: "100%" } } }}>
+					<IconField
+						iconPosition="left"
+						className={styles.input_wrapper}
+					>
+						<InputIcon>
+							<PhoneIcon className={styles.smartphone_icon} />
+						</InputIcon>
+						<InputMask
+							id={name}
+							autoClear={false}
+							ref={ref}
+							name={name}
+							onChange={onChange}
+							onBlur={onBlur}
+							mask={`${dialCode}9 999 9999`}
+							placeholder="05"
+							className={classNames(styles.input, {
+								"p-invalid": error,
+							})}
+							onClick={onClick}
+							value={value}
+							type={type}
+							autoComplete={"tel"}
+							inputMode={"tel"}
+						/>
+						{!disabled && (
+							<div className={styles.checkmark_container}>
+								<i
+									className={`pi pi-check-circle ${styles.checkmark_animation}`}
+								></i>
+							</div>
+						)}
+					</IconField>
+					<label htmlFor={name}>Phone Number</label>
+				</FloatLabel>
 
 				{error && (
-					<small className={`p-error ${styles.animated_error}`}>{error.message}</small>
+					<small className={`p-error ${styles.animated_error}`}>
+						{error.message}
+					</small>
 				)}
 			</div>
 		);
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import path from "path";
 import { fileURLToPath } from "url";
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 const __filename = fileURLToPath(import.meta.url);
 
@@ -12,7 +12,7 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
 	base: "/",
 	publicDir: "public",
-	plugins: [react()],
+	plugins: [react(), svgr()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
